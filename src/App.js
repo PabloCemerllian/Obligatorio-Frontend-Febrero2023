5bf5bd0367e4223b2a069bf718ca69a45f0914e1
Pablo Cabrera Cemerllian <pablo.cabrera91@outlook.com>
2023-04-03 20:44:16 -0300

    creados registro home y login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Cabezal from "./componentes/Cabezal/Cabezal";
+import Pie from "./componentes/Pie";
+import Contenido from "./componentes/Contenido/Contenido";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Cabezal></Cabezal>
+      <Contenido></Contenido>
+      <Pie></Pie>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/componentes/Cabezal/Cabezal.js
@@ -0,0 +1,9 @@
+import React from "react";
+import cabezal from './Cabezal.module.css';
+
+const Cabezal = () => {
+  return (
+  <div className={cabezal.titulo}>Componente Cabezal</div>);
+};
+
+export default Cabezal;
